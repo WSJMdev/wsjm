53e88a87cf9e44617980cff91a80d13ad393cc4f
sojeong <sojeong5879@gmail.com>
2021-02-23 19:17:21 +0900

    뷰 수정

--- src/main/java/com/project/wsjm/config/SecurityConfig.java
+++ src/main/java/com/project/wsjm/config/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
         http.authorizeRequests()
                 // 페이지 권한 설정
                 .antMatchers("/securityAdmin/**").hasRole("ADMIN")
